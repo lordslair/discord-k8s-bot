774fe1f161c8a28fcd7125862b8129cb3e15d0ad
lordslair <5469390+lordslair@users.noreply.github.com>
2023-11-16 17:02:33 +0100

    fix: Undefined names

--- code/subcommands/kubectl/_autocomplete.py
+++ code/subcommands/kubectl/_autocomplete.py
@@ -9,7 +9,7 @@ from loguru import logger
 def k8s_list_namespaced_pod(ctx: discord.AutocompleteContext):
     try:
         config.load_kube_config(f"/etc/kubeconfig.yaml")
-        pods = client.CoreV1Api().list_namespaced_pod(namespace)
+        pods = client.CoreV1Api().list_namespaced_pod(ctx.options["namespace"])
     except Exception as e:
         logger.error(f'K8s Query KO [{e}]')
         return []
