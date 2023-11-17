b4b2612bed57347adf96b78468e9ccb12d9952a4
lordslair <5469390+lordslair@users.noreply.github.com>
2023-11-17 14:55:51 +0100

    feat: Added get commands

--- code/subcommands/kubectl/get.py
+++ code/subcommands/kubectl/get.py
@@ -27,9 +27,10 @@ def get(group_kubectl, bot):
         "resource",
         description="Resource type",
         choices=[
-            discord.OptionChoice('Deployments'),
-            discord.OptionChoice('Pods'),
-            discord.OptionChoice('Services'),
+            discord.OptionChoice('deployments'),
+            discord.OptionChoice('pods'),
+            discord.OptionChoice('pvc'),
+            discord.OptionChoice('services'),
             ],
         )
     async def get(
@@ -37,7 +38,7 @@ def get(group_kubectl, bot):
         namespace: str,
         resource: str,
     ):
-        await ctx.defer()  # To defer answer (default: 15min)
+        await ctx.defer(ephemeral=True)  # To defer answer (default: 15min)
         logger.info(
             f'[#{ctx.channel.name}][{ctx.channel.name}] '
             f'/{group_kubectl} get {namespace} {resource}'
@@ -50,10 +51,10 @@ def get(group_kubectl, bot):
         else:
             pass
 
-        if resource == 'Pods':
+        if resource == 'pods':
             try:
-                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query Starting')
-                resource = client.CoreV1Api().list_namespaced_pod(namespace)
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query')
+                resources = client.CoreV1Api().list_namespaced_pod(namespace)
                 logger.debug(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query Ended')
             except Exception as e:
                 logger.error(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query KO [{e}]')
@@ -63,9 +64,216 @@ def get(group_kubectl, bot):
                     )
                 await ctx.respond(embed=embed)
                 return
-        elif resource == 'Deployments':
-            pass
-        elif resource == 'Services':
-            pass
+            else:
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query OK')
+
+            # We got the resources object, we can start working
+            description = ''
+            embed = discord.Embed(
+                title=f'kubectl get pods [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+
+            # We Start to update the Embed
+            await ctx.interaction.edit_original_response(embed=embed)
+            # We loop over pods to have detailed infos
+            for pod in resources.items:
+                # We skip CronJob generated pods
+                if 'job-name' in pod.metadata.labels:
+                    continue
+
+                description += f"`{pod.metadata.name}`\n"
+                logger.trace(pod.metadata.name)
+
+                for c in pod.status.container_statuses:
+                    if c.ready:
+                        status = '🟩 '
+                    else:
+                        status = '🟥 '
+                    logger.trace(f"> {status} {c.name} ({c.restart_count})")
+                    description += f"> {status} `{c.name} ({c.restart_count})`\n"
+
+            # We update the Embed with the pod result
+            embed = discord.Embed(
+                title=f'kubectl get pods [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+            await ctx.interaction.edit_original_response(embed=embed)
+            return
+        elif resource == 'pvc':
+            try:
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query')
+                resources = client.CoreV1Api().list_namespaced_persistent_volume_claim(namespace)
+                logger.debug(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query Ended')
+            except Exception as e:
+                logger.error(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query KO [{e}]')
+                embed = discord.Embed(
+                    description='Command aborted: K8s Query KO',
+                    colour=discord.Colour.red()
+                    )
+                await ctx.respond(embed=embed)
+                return
+            else:
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query OK')
+
+            if len(resources.items) == 0:
+                msg = 'No pvc were found for this namespace'
+                logger.debug(msg)
+                embed = discord.Embed(
+                    description=msg,
+                    colour=discord.Colour.orange()
+                    )
+                await ctx.respond(embed=embed)
+                return
+
+            # We got the resources object, we can start working
+            description = ''
+            embed = discord.Embed(
+                title=f'kubectl get pvc [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+
+            # We Start to update the Embed
+            await ctx.interaction.edit_original_response(embed=embed)
+            # We loop over pods to have detailed infos
+            for pvc in resources.items:
+                if pvc.status.phase == 'Bound':
+                    status = '🟩 '
+                else:
+                    status = '⬜ '
+                label = f"{pvc.metadata.name} ({pvc.status.capacity['storage']})"
+                description += f"{status} `{label}`\n"
+                logger.trace(f"{status} {label}")
+
+            # We update the Embed with the pod result
+            embed = discord.Embed(
+                title=f'kubectl get pvc [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+            await ctx.interaction.edit_original_response(embed=embed)
+            return
+        elif resource == 'deployments':
+            try:
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query')
+                resources = client.AppsV1Api().list_namespaced_deployment(namespace)
+                logger.debug(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query Ended')
+            except Exception as e:
+                logger.error(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query KO [{e}]')
+                embed = discord.Embed(
+                    description='Command aborted: K8s Query KO',
+                    colour=discord.Colour.red()
+                    )
+                await ctx.respond(embed=embed)
+                return
+            else:
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query OK')
+
+            if len(resources.items) == 0:
+                msg = 'No deployment were found for this namespace'
+                logger.debug(msg)
+                embed = discord.Embed(
+                    description=msg,
+                    colour=discord.Colour.orange()
+                    )
+                await ctx.respond(embed=embed)
+                return
+
+            # We got the resources object, we can start working
+            description = ''
+            embed = discord.Embed(
+                title=f'kubectl get deployment [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+
+            # We Start to update the Embed
+            await ctx.interaction.edit_original_response(embed=embed)
+            # We loop over pods to have detailed infos
+            for deployment in resources.items:
+                if deployment.spec.replicas == deployment.status.available_replicas:
+                    status = '🟩 '
+                else:
+                    status = '🟥 '
+                count = f"{deployment.status.available_replicas}/{deployment.spec.replicas}"
+                description += f"{status} `{deployment.metadata.name} ({count})`\n"
+                logger.trace(f"{status} {deployment.metadata.name} ({count})")
+
+            # We update the Embed with the pod result
+            embed = discord.Embed(
+                title=f'kubectl get deployment [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+            await ctx.interaction.edit_original_response(embed=embed)
+            return
+        elif resource == 'services':
+            try:
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query')
+                resources = client.CoreV1Api().list_namespaced_service(namespace)
+                logger.debug(f'[#{ctx.channel.name}][{ctx.author.name}] ├──> K8s Query Ended')
+            except Exception as e:
+                logger.error(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query KO [{e}]')
+                embed = discord.Embed(
+                    description='Command aborted: K8s Query KO',
+                    colour=discord.Colour.red()
+                    )
+                await ctx.respond(embed=embed)
+                return
+            else:
+                logger.info(f'[#{ctx.channel.name}][{ctx.author.name}] └──> K8s Query OK')
+
+            if len(resources.items) == 0:
+                msg = 'No services were found for this namespace'
+                logger.debug(msg)
+                embed = discord.Embed(
+                    description=msg,
+                    colour=discord.Colour.orange()
+                    )
+                await ctx.respond(embed=embed)
+                return
+
+            # We got the resources object, we can start working
+            description = ''
+            embed = discord.Embed(
+                title=f'kubectl get services [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+
+            # We Start to update the Embed
+            await ctx.interaction.edit_original_response(embed=embed)
+            # We loop over pods to have detailed infos
+            for service in resources.items:
+                description += f"`{service.metadata.name} ({service.spec.type})`\n"
+                logger.trace(f"{service.metadata.name} ({service.spec.type})")
+
+                for service_port in service.spec.ports:
+                    src = f"{service.spec.cluster_ip}:{service_port.port}"
+                    if service.spec.selector:
+                        dst = f"{service.spec.selector['name']}:{service_port.target_port}"
+                    else:
+                        dst = f"None:{service_port.target_port}"
+                    logger.trace(f"> {src} -> {dst}")
+                    description += f"> `{src} -> {dst}`\n"
+
+                # Specific for Loadbalancers
+                if service.spec.type == 'LoadBalancer':
+                    for ingress in service.status.load_balancer.ingress:
+                        logger.trace(f"> hostname: {ingress.hostname}")
+                        description += f"> `hostname: {ingress.hostname}`\n"
+
+            # We update the Embed with the pod result
+            embed = discord.Embed(
+                title=f'kubectl get services [{namespace}]',
+                description=description,
+                colour=discord.Colour.green()
+                )
+            await ctx.interaction.edit_original_response(embed=embed)
+            return
+
         else:
             pass
